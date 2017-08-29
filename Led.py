de153e0d50e1e9fd9e47e3370093e3789b2ce113
DonionZooimama <31424488+DonionZooimama@users.noreply.github.com>
2017-08-29 13:34:51 -0400

    Update Led.py

--- Led.py
+++ Led.py
@@ -5,7 +5,7 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8,GPIO.OUT)
 while True:
-        GPIO.output(8, False)
+        GPIO.output(8, GPIO.HIGH)
         time.sleep(0.1)
         GPIO.output(8, GPIO.LOW)
         time.sleep(0.1)
